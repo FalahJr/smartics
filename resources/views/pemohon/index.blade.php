edd6192e09db079621c405b86fb44f09c3195eeb
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-11-13 03:01:18 +0700

    Feature (backend) : create index pemohon

---
+++ resources/views/pemohon/index.blade.php
@@ -0,0 +1,241 @@
+@extends('main')
+@section('content')
+
+@include('pemohon.tambah')
+<style type="text/css">
+
+</style>
+<!-- partial -->
+<div class="content-wrapper">
+  <div class="row">
+    <div class="col-lg-12">
+      <nav aria-label="breadcrumb" role="navigation">
+        <ol class="breadcrumb bg-warning">
+          <li class="breadcrumb-item"><i class="fa fa-home"></i>&nbsp;<a href="/home">Home</a></li>
+          {{-- <li class="breadcrumb-item">Setup Master Tagihan</li> --}}
+          <li class="breadcrumb-item active" aria-current="page">Pemohon</li>
+        </ol>
+      </nav>
+    </div>
+  	<div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Pemohon</h4>
+                    <div class="col-md-12 col-sm-12 col-xs-12" align="right" style="margin-bottom: 15px;">
+                      {{-- @if(Auth::user()->akses('MASTER DATA STATUS','tambah')) --}}
+                    	
+                      {{-- @endif --}}
+                    </div>
+                    <div class="table-responsive">
+        				        <table class="table table_status table-hover " id="table-data" cellspacing="0">
+                            <thead class="bg-warning text-white">
+                              <tr>
+                                <th>No</th>
+                                <th>Username</th>
+                                <th>Password</th>
+                                <th>Nama Lengkap</th>
+                                <th>Action</th>
+                              </tr>
+                            </thead>
+
+                            <tbody>
+
+                            </tbody>
+
+
+                        </table>
+                    </div>
+                  </div>
+                </div>
+    </div>
+  </div>
+</div>
+<!-- content-wrapper ends -->
+@endsection
+@section('extra_script')
+<script>
+
+var table = $('#table-data').DataTable({
+        processing: true,
+        // responsive:true,
+        serverSide: true,
+        searching: true,
+        paging: true,
+        
+        // dom: 'Bfrtip',
+        title: '',
+        buttons: [
+            // 'pdf'
+        ],
+        ajax: {
+            url:'{{ url('/pemohontable') }}',
+        },
+        columnDefs: [
+
+              {
+                 targets: 0 ,
+                 className: 'center id'
+              },
+              {
+                 targets: 1,
+                 className: 'nominal center'
+              },
+              {
+                 targets: 2,
+                 className: 'type center'
+              },
+              {
+                 targets: 3,
+                 className: 'center'
+              },
+              {
+                 targets: 4,
+                 className: 'center'
+              }
+            ],
+        "columns": [
+          {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+          {data: 'username', name: 'username'},
+          {data: 'password', name: 'password'},
+          {data: 'nama_lengkap', name: 'Nama Lengkap'},
+          {data: 'aksi', name: 'aksi'},
+
+        ]
+  });
+
+
+
+  function edit(id) {
+    // body...
+    $.ajax({
+      url:baseUrl + '/editpemohon',
+      data:{id},
+      dataType:'json',
+      success:function(data){
+        // console.log
+        $('.id').val(data.id);
+        $('.nama_lengkap').val(data.nama_lengkap);
+        $('.username').val(data.username);
+        $('.password').val(data.password);
+      
+
+        
+        
+        // $('.datepicker').val(data.created_at)
+        $('#tambah').modal('show');
+      }
+    });
+
+  }
+
+  $('#simpan').click(function(){
+    $.ajax({
+      url: baseUrl + '/simpanpemohon',
+      data:$('.table_modal :input').serialize(),
+      dataType:'json',
+      success:function(data){
+        if (data.status == 1) {
+          iziToast.success({
+              icon: 'fa fa-save',
+              message: 'Data Berhasil Disimpan!',
+          });
+          reloadall();
+        }else if(data.status == 2){
+          iziToast.warning({
+              icon: 'fa fa-info',
+              message: 'Data Gagal disimpan!',
+          });
+        }else if (data.status == 3){
+          iziToast.success({
+              icon: 'fa fa-save',
+              message: 'Data Berhasil Diubah!',
+          });
+          reloadall();
+        }else if (data.status == 4){
+          iziToast.warning({
+              icon: 'fa fa-info',
+              message: 'Data Gagal Diubah!',
+          });
+        }
+
+      }
+    });
+  })
+
+
+  function hapus(id) {
+    iziToast.question({
+      close: false,
+  		overlay: true,
+  		displayMode: 'once',
+  		title: 'Hapus data',
+  		message: 'Apakah anda yakin ?',
+  		position: 'center',
+  		buttons: [
+  			['<button><b>Ya</b></button>', function (instance, toast) {
+          $.ajax({
+            url:baseUrl + '/hapuspemohon',
+            data:{id},
+            dataType:'json',
+            success:function(data){
+              iziToast.success({
+                  icon: 'fa fa-trash',
+                  message: 'Data Berhasil Dihapus!',
+              });
+
+              reloadall();
+            }
+          });
+  			}, true],
+  			['<button>Tidak</button>', function (instance, toast) {
+  				instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+  			}],
+  		]
+  	});
+  }
+
+  function approve(id) {
+    iziToast.question({
+      close: false,
+  		overlay: true,
+  		displayMode: 'once',
+  		title: 'Approve data pemohon',
+  		message: 'Apakah anda yakin ?',
+  		position: 'center',
+  		buttons: [
+  			['<button><b>Ya</b></button>', function (instance, toast) {
+          $.ajax({
+            url:baseUrl + '/approvepemohon',
+            data:{id},
+            dataType:'json',
+            success:function(data){
+              iziToast.success({
+                  icon: 'fa fa-check',
+                  message: 'Data Pemohon berhasil di approve!',
+              });
+
+              reloadall();
+            }
+          });
+  			}, true],
+  			['<button>Tidak</button>', function (instance, toast) {
+  				instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+  			}],
+  		]
+  	});
+  }
+
+  function reloadall() {
+    $('.table_modal :input').val("");
+
+    $('#tambah').modal('hide');
+
+    // $("#inputtext").val("");
+   
+    // $(".inputtext").val("");
+    // var table1 = $('#table_modal').DataTable();
+    // table1.ajax.reload();
+    table.ajax.reload();
+  }
+</script>
+@endsection
