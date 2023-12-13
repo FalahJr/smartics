956e4072c859991ab1b733f3c49266e176e765ea
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2023-12-13 18:56:01 +0700

    Fix (backend) : surveyor isi jawaban pertanyaan

--- resources/views/survey-penugasan/form-pertanyaan-survey.blade.php
+++ resources/views/survey-penugasan/form-pertanyaan-survey.blade.php
@@ -32,18 +32,15 @@ body {
                         @csrf
                         @method('post')
                         <div class="row">
-                            <input type="hidden" class="form-control" id="id" name="id" value="{{ $suratId->id }}">
+                            <input type="hidden" class="form-control" id="survey_id" name="survey_id" value="{{ $suratId->id }}">
 
                             @foreach ($dataSurveyPertanyaan as $index => $list)
                             <div class="form-group col-6">
-                                <label for="jawaban"  >{{ $index + 1 }}) {{ $list->pertanyaan }}</label>
+                                <label for="jawaban{{ $index }}"  >{{ $index + 1 }}) {{ $list->pertanyaan }}</label>
                                 <input type="hidden" class="form-control" name="survey_pertanyaan_id[]" value="{{ $list->id }}">
-                                <textarea class="form-control mt-2" name="jawaban[]"></textarea>
-
-                               
+                                <textarea class="form-control mt-2" name="jawaban[]" id="jawaban{{ $index }}"></textarea>
                             </div>
-                        @endforeach
-                         
+                            @endforeach
                         </div>
                         <div class="row btn-update-profile mt-4">
                             <button type="button" class="btn btn-main text-light" id="kirim">Kirim Laporan</button>
@@ -58,62 +55,46 @@ body {
 
 @section('extra_script')
 
-
 <script>
-
-
 $('#kirim').click(function() {
-    var formData = {
-        'survey_id': $('#id').val(),
-        'survey_pertanyaan_id': $('[name="survey_pertanyaan_id[]"]').map(function() {
-            return $(this).val();
-        }).get(),
-        'jawaban': $('[name="jawaban[]"]').map(function() {
-            return $(this).val();
-        }).get()
-    };
-
-    console.log(formData);
+    var formData = new FormData($('#form1')[0]);
 
     // Ajax request untuk mengirim data ke backend
-    // $.ajax({
-    //     url: baseUrl + '/isi-survey',
-    //     type: 'POST',
-    //     contentType: 'application/json',
-    //     data: JSON.stringify(formData),
-    //     success: function(data) {
-    //         if (data.status == 1) {
-    //             iziToast.success({
-    //                 icon: 'fa fa-save',
-    //                 message: 'Data Berhasil Disimpan!',
-    //             });
-    //             // let id = data.id;
-    //             window.location.href = baseUrl + '/survey/penugasan;
-
-    //             // reloadall();
-    //         } else if(data.status == 2){
-    //             iziToast.warning({
-    //                 icon: 'fa fa-info',
-    //                 message: data.message,
-    //             });
-    //         } else if (data.status == 3){
-    //             iziToast.success({
-    //                 icon: 'fa fa-save',
-    //                 message: 'Data Berhasil Diubah!',
-    //             });
-    //             reloadall();
-    //         } else if (data.status == 4){
-    //             iziToast.warning({
-    //                 icon: 'fa fa-info',
-    //                 message: 'Data Gagal Diubah!',
-    //             });
-    //         }
-    //     },
-    //     error: function(error) {
-    //         // Handle kesalahan
-    //         console.error(error);
-    //     }
-    // });
+    $.ajax({
+        url: baseUrl + '/isi-survey',
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function(data) {
+            if (data.status == 1) {
+                iziToast.success({
+                    icon: 'fa fa-save',
+                    message: 'Data Berhasil Disimpan!',
+                });
+                window.location.href = baseUrl + '/survey/penugasan';
+            } else if (data.status == 2) {
+                iziToast.warning({
+                    icon: 'fa fa-info',
+                    message: data.message,
+                });
+            } else if (data.status == 3) {
+                iziToast.success({
+                    icon: 'fa fa-save',
+                    message: 'Data Berhasil Diubah!',
+                });
+                reloadall();
+            } else if (data.status == 4) {
+                iziToast.warning({
+                    icon: 'fa fa-info',
+                    message: 'Data Gagal Diubah!',
+                });
+            }
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 });
 </script>
 @endsection
